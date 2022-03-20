54e5c5c9f383ace1c65c32654a985b68ac30b266
llyang1 <46336106+llyang1@users.noreply.github.com>
2022-03-20 17:45:51 +0800

    Add files via upload

---
+++ yll-README.md
@@ -0,0 +1,19 @@
+## azpanel
+演示站点：[https://azpanel.net](https://azpanel.net)
+
+允许注册并正常使用
+## telegram
+频道：[https://t.me/azpanel](https://t.me/azpanel)
+
+群聊：[https://t.me/+w_DuAFGop4kzOGYx](https://t.me/+w_DuAFGop4kzOGYx)
+## 安装教程
+- lnmp.org
+
+[https://github.com/azpanel/azpanel/wiki/lnmp.org](https://github.com/azpanel/azpanel/wiki/lnmp.org)
+
+- bt.cn
+
+[https://github.com/azpanel/azpanel/wiki/bt.cn](https://github.com/azpanel/azpanel/wiki/bt.cn)
+
+## 更新
+curl -sS https://raw.githubusercontent.com/llyang1/azpanel/master/update_azpanel.sh | bash
---
+++ yll-update_azpanel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# chmod +x update_azpanel.sh
+
+iName=$(find / -iname upgrade.sh | xargs grep -li 'checkoutConfirm()')
+iPath=$(echo $iName | sed 's_upgrade.sh__')
+cd $iPath; echo "yes" | bash upgrade.sh;
+composer install;composer update;php think migrate:run;cd #防止报错
+
+#使用英文win11/win10
+myPHP=$(find / -iname "AzureList.php"); echo $myPHP
+sed -i 's_win11-21h2-pro-zh-cn_win11-21h2-entn_' $myPHP
+sed -i 's_win10-21h2-pro-zh-cn-g2_win10-21h2-entn-g2_' $myPHP
+#lnmp restart
+
+#json格式修改
+myPHP=$(find / -iname "*.php" | xargs grep -li "subscriptionId" | grep -v temp)
+sed -i "/configs[[]/s_appId[']_clientId\'_" $myPHP
+sed -i "/configs[[]/s_tenant[']_tenantId\'_" $myPHP
+sed -i "/configs[[]/s_password[']_clientSecret\'_" $myPHP
+grep "clientId" $myPHP;grep "tenantId" $myPHP;grep "clientSecret" $myPHP
+
+#让每行显示100条。默认15
+iName=$(find / -iname .env | xargs grep -li 'APP_NAME')
+sed -i '/APP_NAME/ s;$;\nPAGINATE = 100;' $iName
+#sed '/APP_NAME/ s;^;PAGINATE = 100\n;' $iName
+
+lnmp restart
